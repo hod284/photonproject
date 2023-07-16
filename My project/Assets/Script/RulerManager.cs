e1fac845cac203505102cfd19188ff9985b0fa15
hod284 <61046278+hod284@users.noreply.github.com>
2023-07-17 01:02:27 +0900

    rulerfltptxld
    \룰러데셋칭

--- My project/Assets/Script/RulerManager.cs
+++ My project/Assets/Script/RulerManager.cs
@@ -16,7 +16,7 @@ public class RulerManager : MonoBehaviour
     public TMP_Text nowdismater;
     private RulerObjST _nowRulerObj;
     private List<RulerObjST> _rulerObjList =new List<RulerObjST>();
-    private bool _rulerEnable;
+    private bool _rulerEnable=false;
     private Vector3 _rulerPosSave;
     // Start is called before the first frame update
     void Start()
@@ -29,24 +29,28 @@ public class RulerManager : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            _centerVec = Input.GetTouch(0).position;
-            MakeRulerObj();
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    _centerVec = Input.GetTouch(0).position;
+                    RayCast();
+                    MakeRulerObj();
+                }
+                else if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+                    _centerVec = Input.GetTouch(0).position;
+                    RayCast();
+                }
+                else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    _centerVec = Input.GetTouch(0).position;
+                    RayCast();
+                    MakeRulerObj();
+                }
         }
         else
-            _centerVec = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
-        if (m_RaycastManager.Raycast(_centerVec, s_Hits, TrackableType.PlaneWithinPolygon))
         {
-           
-                var hitPose = s_Hits[0].pose; // 첫번째로 측정된 면의 정보를 가져옴.
-                _rulerPosSave = hitPose.position;
-
-                if (_nowRulerObj != null&& _rulerEnable)
-                {
-                    _nowRulerObj.SetObj(hitPose.position);
-                    _nowRulerObj.checkline();
-                     nowdismater.text = _nowRulerObj._text.text;
-                }
-            
+            _centerVec = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+            RayCast();
         }
         
     }
@@ -74,7 +78,34 @@ public class RulerManager : MonoBehaviour
                  _rulerEnable = false;
                  _nowRulerObj = null;
                  nowdismater.gameObject.SetActive(false);
+                 _rulerPosSave = Vector3.zero;
             }
         
     }
+    public void RayCast()
+    {
+        if (m_RaycastManager.Raycast(_centerVec, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+
+            var hitPose = s_Hits[0].pose; // 첫번째로 측정된 면의 정보를 가져옴.
+            _rulerPosSave = hitPose.position;
+
+            if (_nowRulerObj != null && _rulerEnable)
+            {
+                _nowRulerObj.SetObj(hitPose.position);
+                _nowRulerObj.checkline();
+                nowdismater.text = _nowRulerObj._text.text;
+            }
+
+        }
+    }
+    public void listreset()
+    {
+        for(int i =0; i< _rulerObjList.Count; i++)
+            Destroy(_rulerObjList[i].gameObject);
+        _rulerObjList.Clear();
+        _nowRulerObj = null;
+        _rulerPosSave = Vector3.zero;
+    }
+  
 }
